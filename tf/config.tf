4d1a08a9c8d3fdaa471481d39254a9fa402ad1b7
Christian Johansen <christian@cjohansen.no>
2025-01-19 20:49:46 +0100

    Add terraform setup

---
+++ tf/config.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-cjohansen"
+    key = "replicant.docs/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
