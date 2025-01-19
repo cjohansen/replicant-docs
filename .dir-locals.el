073bc6a1484a20cf3c9dfe51a65c5fdccd17dcf4
Christian Johansen <christian@cjohansen.no>
2025-01-19 12:32:31 +0100

    Switch all cljs builds to shadow

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil
   (cider-clojure-cli-aliases . "-A:dev")
   (cider-preferred-build-tool . clojure-cli)
-  (cider-default-cljs-repl . figwheel-main)
-  (cider-figwheel-main-default-options . ":dev")))
+  (cider-default-cljs-repl . shadow)
+  (cider-shadow-watched-builds . (":client" ":portfolio"))))
